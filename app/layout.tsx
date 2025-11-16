f110db9e206d953ee449a3f200d34f54920c2192
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-11-16 15:49:05 +0530

    feat: add QueryClientProvider for React Query integration and update layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/provider/AuthProvider";
+import QueryClientProvider from "@/provider/queryClientProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,9 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </QueryClientProvider>
 
       </body>
     </html>
