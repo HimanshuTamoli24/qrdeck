3ecb8b041b1bf2b920e0d83f593a50372fec74a0
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-11-21 03:20:12 +0530

    feat: replace QueryClientProvider with QueryClientsProvider and remove unused queryClientProvider component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/provider/AuthProvider";
-import QueryClientProvider from "@/provider/QueryClientProvider";
 import Header from "@/components/ui/custom/header";
 import { ThemeProviders } from "@/provider/ThemeProvider";
+import QueryClientsProvider from "@/provider/QueryClientProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -30,14 +30,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QueryClientProvider>
+        <QueryClientsProvider>
           <AuthProvider>
             <ThemeProviders>
             <Header />
             {children}
             </ThemeProviders>
           </AuthProvider>
-        </QueryClientProvider>
+        </QueryClientsProvider>
+       
 
       </body>
     </html>
